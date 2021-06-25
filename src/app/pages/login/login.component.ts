029aca5921e27cc5bb7bdf6f561b44d373cf10dc
Gustavo Blanco Alfaro <32990052+Tavo506@users.noreply.github.com>
2021-06-25 16:22:33 -0600

    Cosas que me fallaron ptm

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/model/Usuario';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +12,65 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  //Model usuario
+  //Model usuario
+  usuario: Usuario = this.Usuario();
+  recordarme: boolean = false;
 
-  ngOnInit(): void {
+  constructor( private auth: AuthService,
+               private router: Router ) {}
+
+  ngOnInit() {
+    // en el LS hay un email
+    if ( localStorage.getItem('email') ) {
+      this.recordarme = true;
+    }
+
+  }
+
+  Usuario(){
+    return {
+      email : "",
+      password : "",
+      nombre : ""
+    }
+  }
+
+  login( form: NgForm ) {
+    // si el form está ok
+    if (  form.invalid ) { return; }
+
+    //alert de loading
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+    setTimeout(() => {
+      Swal.close();
+    }, 500);
+
+    // //hace el login con el servicio
+    // this.auth.login( this.usuario )
+    //   .subscribe( resp => {
+    //     console.log(resp);
+    //     Swal.close();
+
+    //     if ( this.recordarme ) {
+    //       localStorage.setItem('email', this.usuario.email);
+    //     }
+    //     this.router.navigateByUrl('/home');
+    //     // si da error
+    //   }, (err) => {
+    //     console.log(err.error.error.message);
+    //     Swal.fire({
+    //       type: 'error',
+    //       title: 'Error al autenticar',
+    //       text: err.error.error.message
+    //     });
+    //   });
+
   }
 
 }
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Usuario } from 'src/app/model/Usuario';
+import { AuthService } from 'src/app/services/auth.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +12,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  usuario: Usuario = this.Usuario();
+  recordarme = false;
 
-  ngOnInit(): void {
+  constructor( private auth: AuthService,
+               private router: Router ) { }
+
+  ngOnInit() {
+  }
+
+  Usuario(){
+    return {
+      email : "",
+      password : "",
+      nombre : ""
+    }
+  }
+
+  onSubmit( form: NgForm ) {
+
+    if ( form.invalid ) { return; }
+
+    Swal.fire({
+      allowOutsideClick: false,
+      icon: 'info',
+      text: 'Espere por favor...'
+    });
+    Swal.showLoading();
+    Swal.close();
+
+    // this.auth.nuevoUsuario( this.usuario )
+    //   .subscribe( resp => {
+
+    //     console.log(resp);
+    //     Swal.close();
+
+    //     if ( this.recordarme ) {
+    //       localStorage.setItem('email', this.usuario.email);
+    //     }
+
+    //     this.router.navigateByUrl('/home');
+
+    //   }, (err) => {
+    //     console.log(err.error.error.message);
+    //     Swal.fire({
+    //       type: 'error',
+    //       title: 'Error al autenticar',
+    //       text: err.error.error.message
+    //     });
+    //   });
   }
 
 }
